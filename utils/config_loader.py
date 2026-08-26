25fd9b5634fd1750d59f9e89c35bd18eb2b05e30
Vicky-daa <vigneshblaze11@gmail.com>
2026-08-26 10:09:17 +0530

    added doc analyser code

--- utils/config_loader.py
+++ utils/config_loader.py
@@ -1,7 +1,12 @@
 import yaml
+from logger.custom_logger import CustomLogger
 
-def load_config(config_path:str = "config\config.yaml") ->dict:
+def load_config(config_path:str = "config\config.yaml") -> dict:
     with open(config_path,"r") as file:
         config = yaml.safe_load(file)
     return config 
 
+
+
+logger = CustomLogger().get_logger(__file__)
+logger.info(load_config())
